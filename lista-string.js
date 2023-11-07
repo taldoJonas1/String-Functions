996fe2aabb56c0adcdadbfb691c819b4ff107372
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-11-07 19:35:46 -0300

    Update lista-string.js

--- lista-string.js
+++ lista-string.js
@@ -1,68 +1,68 @@
-function ex1(){
-// Calcule o comprimento de uma string (não use a função strlen).
-    let input = prompt('Insira a string')
-    let i = 0
-    while(input[i] != undefined){
-        i++
-    }
-    console.log(`Comprimento: ${i}`)
-}
+let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length1 = text.length;
+console.log(length1)
+// output: 26
+// lenght mostra o tamanho da string
 
+let text2 = "Apple, Banana, Kiwi";
+let part2 = text.slice(2, 13);
+console.log(part2)
+// output: ple, Banana
+// slice reparte a string com .slice(start, end) start tem preferencia
 
-function ex2(){
-// Leia um nome e imprima o nome somente se a primeira letra do nome for 
-// ‘a’ (maiúscula ou minúscula).
-    let input = prompt('Insira seu nome, ele só será impresso se começar com "A"').toUpperCase()
-    if(input[0] == 'A'){
-        console.log(input)
-    }
-}
+let str3 = "Apple, Banana, Kiwi";
+let part3 = str.substring(-10, 13); // = (0, 13)
+console.log(part3)
+// output: Apple, Banana 
+// The difference is that start and end values less than 0 are treated as 0 in substring().
 
-function ex3(){
-// Leia um nome e imprima as 4 primeiras letras do nome.
-    let input = prompt('Insira seu nome')
-    let answer = input.slice(0,4)
-    console.log(answer)
-}
+let str4 = "Apple, Banana, Kiwi";
+let part4 = str1.substr(7, 10); // = .slice(7, 17)
+console.log(part4)
+// output: Banana, Ki
+// substr começa a contar no primeiro número no parenteses e vai da até a soma com o segundo
+// .substr(i, j) == .slice(i, i+j)
 
-function ex4(){
-// Digite um nome, calcule e retorne quantas letras tem esse nome.
-    let input = prompt('Insira seu nome')
-    let text = input.split(' ')
-    let answer = ''
-    text.forEach(element => {
-        answer = answer + element
-    });
-    console.log(input)
-    console.log(answer.length)
-}
+let text5 = "Please visit Microsoft and Microsoft!";
+let newText5 = text2.replace("Microsoft", "W3Schools");
+console.log(newText5)
+// output: Please visit W3Schools and Microsoft!
+// .replace(a, b)
+// replace troca SOMENTE A PRIMEIRA APARIÇÃO de b por a
+let text6 = "Please visit Microsoft!";
+let newText6 = text.replace(/MICROSOFT/i, "W3Schools");
+// output: Please visit W3Schools!
+// a /i flag torna o termo insensitive, isso significa que a ou A são a mesma coisa
+let text7 = "Please visit Microsoft and Microsoft!";
+let newText7 = text.replace(/Microsoft/g, "W3Schools");
+// output: Please visit W3Schools and W3Schools!
+// a /g flag substitui TODAS AS APARIÇÕES de b por a
 
-function ex5(){
-// Leia nome, sexo e idade. Se sexo for feminino e idade menor que 
-// 25, imprime o nome da pessoa e a palavra “ACEITA”, caso contrário 
-// imprimir “NÃO ACEITA”.
-    let nome = prompt('Insira seu nome')
-    let sexo = prompt('Insira seu sexo [F]eminino ou [M]asculino').toUpperCase()
-    while(sexo.length > 1){
-        sexo = prompt('Por favor, digite apenas uma letra (F ou M)').toUpperCase()
-    }
-    let idade = Number(prompt('Insira sua idade'))
-    if(sexo == 'F'){
-        if(idade < 25){
-            console.log(`${nome} ACEITA`)
-        }
-    }
-    else{
-        console.log('NÃO ACEITA')
-    }
-}
+let text8 = "Hello";
+let str8 = "World";
+let newText8 = text8.concat(" ", str8);
+console.log(newText8)
+// output: Hello World
+// text8.concat(' qualquer coisa ', str8) = Hello qualquer coisa World
+// concatena duas strings
 
-function ex9(){
-// Receba uma palavra e a imprima de trás-para-frente.
-    let palavra = prompt('Insira uma palavra')
-    let novaPalavra = ''
-    for(i = palavra.length - 1; i > - 1; i--){
-        novaPalavra += palavra[i]
-    }
-    console.log(novaPalavra)
-}
+let text9 = "      Hello World!      ";
+let newText9 = text1.trim();
+console.log(newText9)
+// output: Hello World!
+// .trim() tira os whitespaces de ambos os lados
+// .trimStart() tira os whitespaces do começo
+// .trimEnd() tira os whitespaces do fim
+
+let text10 = "5";
+let padded10 = text.padStart(4,"x");
+console.log(padded10)
+// output: xxx5
+// .padStart(repetir n vezes + 1, oq será repetido)
+
+let text11 = "5";
+let padded11 = text.padEnd(4,"x");
+console.log(padded11)
+// output: 5xxx
+// .padEnd(repetir n vezes + 1, oq será repetido)
+// se o text11 for um numero usar .toString para transformar em texto
